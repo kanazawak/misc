2bed6ab214987c07495f657cf633410038d955dd
kanazawak <steelydon.k@gmail.com>
2025-06-05 01:04:44 +0900

    update

--- err2qf.pl
+++ err2qf.pl
@@ -2,15 +2,24 @@
 
 local $| = 1;
 
+sub call {
+  my ($func, $args) = @_;
+  print "\x1b]51;[\"call\",\"Tapi_$func\",[$args]]\x07";
+}
+
+call 'qfopen';
+
 while (<>) {
   print;
   if (/Building\.\.\./) {
     print "\x1b]51;[\"call\",\"Tapi_qfclear\",[]]\x07";
+    call 'qfclear';
   } elsif (/^(.+.cs)\((\d+),(\d+)\): error (.+?)( \[.+\])?$/) {
     my $file = $1;
     my $lnum = $2;
     my $col  = $3;
     my $text = $4;
-    print "\x1b]51;[\"call\",\"Tapi_qfadd\",[\"$file\",$lnum,$col,\"$text\"]]\x07";
+    print "\x1b]51;[\"call\",\"Tapi_qfadd\",[]\x07";
+    call 'qfadd', "\"$file\",$lnum,$col,\"$text\"";
   }
 }
